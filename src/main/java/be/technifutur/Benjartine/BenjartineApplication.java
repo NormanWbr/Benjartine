e2211233a0476bd50bea2fcb65a9543021a6ad97
Norman Wamberchies <norman@wamberchies.be>
2023-02-06 16:39:41 +0100

    first commit

---
+++ src/main/java/be/technifutur/Benjartine/BenjartineApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.Benjartine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenjartineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenjartineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technifutur/Benjartine/BenjartineApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.Benjartine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenjartineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
